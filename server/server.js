9af1afed44cb433399f3342a4ac3064dd9d797e0
Mateusz Pyzowski <managervcf@gmail.com>
2019-02-16 22:09:16 +0800

    Drop experimental modules. Setup simple GraphQL and Apollo server with mock data.

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Import helpers from dependencies
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+// Import files
+const books = require('./graphql/sampleData');
+const typeDefs = require('./graphql/schema');
+const resolvers = require('./graphql/resolvers');
+
+// Variable declarations
+const port = process.env.PORT || 4000;
+
+// Create express server
+const app = express();
+
+// Create apollo server and connect it with express
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+// Routes
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Setup app to listen on variable 'port'
+app.listen(port, () =>
+	console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+);
