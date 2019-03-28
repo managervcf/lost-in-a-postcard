aaf6ee074d3a2e05f5065201a42aa6238a5d5dc9
Mateusz Pyzowski <managervcf@gmail.com>
2019-03-28 22:08:12 +0800

    Shuffle fetched photos array before rendering. Remove unnecessary utils.

--- src/client/src/components/PhotoGallery.js
+++ src/client/src/components/PhotoGallery.js
@@ -6,15 +6,20 @@ import PhotoItem from './PhotoItem';
 import PhotoGalleryDescription from './PhotoGalleryDescription';
 import LoaderBlock from './LoaderBlock';
 import ErrorMessage from './ErrorMessage';
+
 import { PHOTOS } from '../graphql/queries';
+import { shuffle } from '../utils';
 
 const PhotoGallery = ({ location, match }) => {
 	// Build a query depending on url.
 	let query = {};
+	// If url match is not exact, pull out parameters.
 	if (!match.isExact) {
+		// If url contains featured, add featured = true as a variable.
 		if (location.pathname === '/photos/featured') {
 			query.featured = true;
 		} else {
+			// If url is not exact and it is not featured, add as country.
 			query.country = location.pathname.replace(`${match.path}/`, '');
 		}
 	}
@@ -24,15 +29,14 @@ const PhotoGallery = ({ location, match }) => {
 			{({ data, loading, error }) => {
 				if (error) return <ErrorMessage text="Cannot load gallery :(" />;
 				if (loading) return <LoaderBlock size={5} loading={loading} />;
+				if (data.photos.docs.length === 0)
+					return <ErrorMessage text="No photos found :(" />;
 
-				let galleryItems =
-					data.photos.docs.length > 0 ? (
-						data.photos.docs.map((photo, index) => (
-							<PhotoItem key={photo.id} {...photo} />
-						))
-					) : (
-						<ErrorMessage text="No photos found :(" />
-					);
+				let shuffledPhotos = shuffle(data.photos.docs);
+
+				let galleryItems = shuffledPhotos.map((photo, index) => (
+					<PhotoItem key={photo.id} {...photo} />
+				));
 
 				return (
 					<section className="gallery">
--- src/client/src/utils/countryDescriptions.js
+++
@@ -1,5 +0,0 @@
-export default {
-	Indonesia:
-		'17,5 tysięcy wysp, 260 milionów mieszkańców, ponad 300 grup etnicznych - Indonezja działa jak magnes na turystów zaintrygowanych jej ogromną różnorodnością.',
-	Laos: 'Piękny ten Laos. Dużo wodospadów.',
-};
