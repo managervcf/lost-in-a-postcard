8391c26548d7449897acc6513952e39a7088a5cb
Mateusz <managervcf@gmail.com>
2019-11-10 18:03:46 +0100

    Fix broken file upload feature. Drop createReadStream() method coming from incoming uploaded file as it throws fs-capacitor, exceeded stack limit error. Instead, use built-in fs (File System) default node package and fs.createReamStream() function. Unfortunately the path is fixed to desktop at the momemt, so it is just a temporary solution.

--- src/utils/cloudinaryHelpers.js
+++ src/utils/cloudinaryHelpers.js
@@ -1,66 +1,83 @@
 // Import cloudinary for file uploads.
 import cloudinary from 'cloudinary';
+// Import a function from File System nodejs module that allows a file to be sent.
+import { createReadStream } from 'fs';
+// The problem with js.createReamStream(<path/filename>) is that in the browser
+// I don't have access to filepath. Filepath will be fixed to desktop.
 
 // Configure cloudinary API.
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_NAME,
-	api_key: process.env.CLOUDINARY_KEY,
-	api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 
 export const deleteAsset = async public_id => {
-	let result = await cloudinary.uploader.destroy(public_id);
-	console.log(`(Cloudinary) Deleted asset ${public_id}.`);
+  let result = await cloudinary.uploader.destroy(public_id);
+  console.log(`(Cloudinary) Deleted asset ${public_id}.`);
 };
 
 export const uploadAsset = async ({ file, country }, author) => {
-	// Get metadata from uploading file.
-	let { createReadStream, filename, mimetype, encoding } = await file;
+  // Get metadata from uploading file.
+  // Not extracting createReadStream function as it throws fs-capacitor error exceeding stack
+  // limit when called. Use createReadStream from 'fs' (File System) built-in node module instead.
+  let { filename, mimetype, encoding } = await file;
+  // File validation.
+  if (!mimetype.includes('image/')) throw new Error('File must be an image!');
 
-	// File validation.
-	if (!mimetype.includes('image/')) throw new Error('File must be an image!');
+  // Create filepath essential for createReamStream function.
+  // As there is no access to file path in input tag all files will be uploaded from desktop.
+  // Temporary solution!
+  const filepath = `C:\\Users\\managervcf\\Desktop\\${filename}`;
 
-	// Create variable stream.
-	const stream = createReadStream();
+  // Print file details.
+  console.log('<<<< UPLOADED PHOTO >>>>');
+  console.log('Filepath =====', filepath);
+  console.log('Filename =====', filename);
+  console.log('Country =====', country);
+	console.log('Author =====', author);
+	
+  // Create readableStream with provided filepath.
+  const readableStream = createReadStream(filepath);
 
-	// Create variable photo.
-	let photo = {};
+  // Create variable photo.
+  let photo = {};
 
-	// Handle upload to cloudinary.
-	const cloudinaryUpload = async stream => {
-		try {
-			await new Promise((resolve, reject) => {
-				const streamLoad = cloudinary.v2.uploader.upload_stream(
-					{
-						// Put file in lostinapostcard folder.
-						folder: 'lostinapostcard/',
-						// Apply tags.
-						tags: [country, author]
-					},
-					(error, result) => {
-						if (result) {
-							photo.url = result.secure_url;
-							photo.public_id = result.public_id;
-							console.log(`(Cloudinary) Uploaded new photo at ${photo.url}.`);
-							resolve(photo);
-						} else {
-							reject(error);
-						}
-					}
-				);
+  // Define function handling upload to cloudinary.
+  const cloudinaryUpload = async stream => {
+    try {
+      await new Promise((resolve, reject) => {
+        const streamLoad = cloudinary.v2.uploader.upload_stream(
+          {
+            // Put file in lostinapostcard folder.
+            folder: 'lostinapostcard/',
+            // Apply tags.
+            tags: [country, author]
+          },
+          (error, result) => {
+            if (result) {
+              photo.url = result.secure_url;
+              photo.public_id = result.public_id;
+              console.log(`(Cloudinary) Uploaded new photo at ${photo.url}.`);
+              resolve(photo);
+            } else {
+              reject(error);
+            }
+          }
+        );
 
-				stream.pipe(streamLoad);
-			});
-		} catch (error) {
-			throw new Error(
-				`(Cloudinary) Failed to upload photo! Error:${error.message}`
-			);
-		}
-	};
+        stream.pipe(streamLoad);
+      });
+    } catch (error) {
+      throw new Error(
+        `(Cloudinary) Failed to upload photo! Error:${error.message}`
+      );
+    }
+  };
 
-	// Upload photo.
-	await cloudinaryUpload(stream);
+  // Call function handling upload to cloudinary.
+  await cloudinaryUpload(readableStream);
 
-	// Return photo url.
-	return photo;
+  // Return photo url.
+  return photo;
 };
