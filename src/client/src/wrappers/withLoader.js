7cc6281c5e4c3649686053f28c6dbc35718a02fb
Mateusz <managervcf@gmail.com>
2019-12-26 13:00:02 +0100

    Clean up animation wrapper component.

--- src/client/src/wrappers/withLoader.js
+++ src/client/src/wrappers/withLoader.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 
+// Create a wrapping component, that adds a div around
+// passed component and gives it animation class so it
+// can animate itself in.
 const withLoader = WrappedComponent => {
   return class extends Component {
-    state = { didMount: false };
+    // Define a constructor so we can use didMount state to false.
+    constructor(props) {
+      super(props);
+      this.state = { didMount: false };
+    }
 
+    // When component mounts, set didMount to true instantly.
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({ didMount: true });
-      }, 10);
+      setTimeout(() => this.setState({ didMount: true }), 0);
     }
 
     render() {
-      let { didMount } = this.state;
-      let loadingClasses = classnames({
+      // Construct wrapper animation classes.
+      let animateClasses = classnames({
         'fade-in': true,
-        visible: didMount
+        visible: this.state.didMount
       });
+
       return (
-        <div className={loadingClasses}>
+        <div className={animateClasses}>
           <WrappedComponent {...this.props} />
         </div>
       );
