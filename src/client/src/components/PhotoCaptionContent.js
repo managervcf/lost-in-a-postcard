9af49334b786db7400145556f9728edd9125243f
Mateusz <managervcf@gmail.com>
2019-12-30 12:45:18 +0100

    Clean up component.

--- src/client/src/components/PhotoCaptionContent.js
+++ src/client/src/components/PhotoCaptionContent.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import withLoader from '../wrappers/withLoader';
-
-const PhotoCaptionContent = ({ country, caption }) => (
+const PhotoCaptionContent = ({ country: { name }, caption }) => (
   <div className="gallery-caption-content">
     <span className="gallery-caption-title">
-      <Link to={`/photos/${country ? country.name : ''}`}>
-        {country ? country.name : ''}
-      </Link>
+      <Link to={`/photos/${name.toLowerCase()}`}>{name}</Link>
     </span>
     <span className="gallery-caption-description">{caption}</span>
   </div>
 );
 
-export default withLoader(PhotoCaptionContent);
+export default PhotoCaptionContent;
