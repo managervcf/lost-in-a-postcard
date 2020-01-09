613125880f6614f721cc7d34bef1b7562e4ab85f
Mateusz <managervcf@gmail.com>
2020-01-09 19:55:12 +0100

    Change serving static files condition.

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 // Express will serve up production assets
 // Express will serve up the index.html file
 // if it doesn't recognize the route.
-if (true) {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
