85b77a53a4276d8aa82bcd90eed634e1334a776a
Mateusz Pyzowski <managervcf@gmail.com>
2020-04-14 23:43:53 +0800

    Add useOnScreen custom hook.

--- client/src/components/PhotoImage.js
+++ client/src/components/PhotoImage.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classnames from 'classnames';
 import { transformUrl } from '../utils';
+import { useOnScreen } from '../hooks/use-on-screen';
 
 function PhotoImage({ upload, country, visible }) {
+  const ref = useRef();
+  // Call the hook passing in ref and root margin
+  // In this case it would only be considered onScreen if more ...
+  // ... than 300px of element is visible.
+  const onScreen = useOnScreen(ref, '-300px');
   const imageClasses = classnames({
     'gallery-image': true,
     dim: visible,
@@ -11,7 +17,15 @@ function PhotoImage({ upload, country, visible }) {
   // Transform URL based on provided app options.
   const newUrl = transformUrl(upload.url);
 
-  return <img className={imageClasses} src={newUrl} alt={country.name} />;
+  return (
+    <img
+      ref={ref}
+      className={imageClasses}
+      src={newUrl}
+      alt={country.name}
+      // style={{ opacity: onScreen ? '0.2' : '1' }}
+    />
+  );
 }
 
 export default PhotoImage;
