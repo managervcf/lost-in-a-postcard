cc57e5c4596f76e647e38035a971313b4e593257
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-02 11:35:33 +0800

    Move emailRegex to config directory from utils.

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,7 @@
 // Backend options.
+// Email regex.
+export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const maxFileSize = 30000000; // Max upload size in bytes.
 export const maxFiles = 50; // Number of files sent at once.
 export const cloudinaryFolderName = 'lostinapostcard/'; // Cloudinary upload destination folder.
-export const requestedPhotosLimit = 50; // Default number of requested photos.
+export const requestedPhotosLimit = 30; // Default number of requested photos.
