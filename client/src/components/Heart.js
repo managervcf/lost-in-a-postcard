6aa55d58e3e973a2f184cca4f7dfbfa3f622ce5d
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-06 19:23:31 +0800

    Add the click counter inside the Heart component.

--- client/src/components/Heart.js
+++ client/src/components/Heart.js
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
 import { useMutation } from 'react-apollo';
 import { CLICK_PHOTO } from '../graphql/mutations';
+import { PHOTOS } from '../graphql/queries';
 
-const Heart = ({ id }) => {
+const Heart = ({ id, clicks }) => {
   const [clicked, setClicked] = useState(false);
-  const [clickPhoto, { loading, error, data }] = useMutation(CLICK_PHOTO);
+  const [clickPhoto] = useMutation(CLICK_PHOTO, {
+    refetchQueries: [{ query: PHOTOS }],
+  });
 
   const showFilled = () => setClicked(true);
   const showEmpty = () => setClicked(false);
 
   return (
-    <svg
-      className="heart"
-      onPointerLeave={showEmpty}
-      onPointerDown={showFilled}
-      onPointerUp={() => {
-        clickPhoto({ variables: { id } });
-        showEmpty();
-      }}
-    >
-      <use
-        xlinkHref={`./assets/icons/icons.svg#icon-heart${
-          clicked ? '' : '-outlined'
-        }`}
-      ></use>
-    </svg>
+    <div className="heart">
+      <p className="heart-counter">{clicks}</p>
+      <svg
+        className="heart-icon"
+        onPointerLeave={showEmpty}
+        onPointerDown={showFilled}
+        onPointerUp={() => {
+          clickPhoto({ variables: { id } });
+          showEmpty();
+        }}
+      >
+        <use
+          xlinkHref={`./assets/icons/icons.svg#icon-heart${
+            clicked ? '' : '-outlined'
+          }`}
+        ></use>
+      </svg>
+    </div>
   );
 };
 
--- client/src/graphql/queries/photos.js
+++ client/src/graphql/queries/photos.js
@@ -11,6 +11,7 @@ export default gql`
         }
         caption
         featured
+        clicks
         upload {
           url
         }
