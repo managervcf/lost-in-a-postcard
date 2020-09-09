5cb1065fc2813c351645846e3e2e5ded1abeba4d
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-09 21:10:17 +0800

    Fix compilation warnings.

--- client/src/hooks/useOnScreen.js
+++ client/src/hooks/useOnScreen.js
@@ -6,6 +6,8 @@ export function useOnScreen(ref, rootMargin = '0px') {
   const [isIntersecting, setIntersecting] = useState(false);
 
   useEffect(() => {
+    const element = ref.current;
+
     const observer = new IntersectionObserver(
       ([entry]) => {
         // Update our state when observer callback fires
@@ -16,11 +18,12 @@ export function useOnScreen(ref, rootMargin = '0px') {
       }
     );
     if (ref.current) {
-      observer.observe(ref.current);
+      observer.observe(element);
     }
     return () => {
-      observer.unobserve(ref.current);
+      observer.unobserve(element);
     };
+    // eslint-disable-next-line
   }, []); // Empty array ensures that effect is only run on mount and unmount
 
   return isIntersecting;
