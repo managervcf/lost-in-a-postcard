6e19dcc8824d10dcdc93ee111777a9f72c3e9b2a
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-15 20:33:44 +0800

    Add emailRegex config variable.

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,7 @@
 // Backend options.
 // Email regex.
 export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const imageRegex = /\.(jpe?g|png|gif|bmp)$/i; // Image regex.
 export const maxFileSize = 30000000; // Max upload size in bytes.
 export const maxFiles = 50; // Number of files sent at once.
 export const cloudinaryFolderName = 'lostinapostcard/'; // Cloudinary upload destination folder.
