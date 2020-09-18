708d21cbcc42298cb7aaab195f58bc079c503f05
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-18 20:33:44 +0800

    Further improve test suites.

--- test/gallery.test.js
+++ test/gallery.test.js
@@ -3,6 +3,9 @@ import { CustomPage } from './helpers/page';
 /**
  * Define page variable in the global scope
  * to make it accessible inside the tests.
+ * @beforeEach - Open the browser and create
+ *               a new tab inside chromium.
+ * @afterEach - Close the browser.
  */
 let page;
 
@@ -11,7 +14,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await page.close(false);
+  await page.close();
 });
 
 /**
@@ -19,21 +22,13 @@ afterEach(async () => {
  */
 it('renders the gallery description', async () => {
   /**
-   * 1. Navigate to /photos/featured.
+   * 1. Navigate to /.
    * 2. Define a selector.
-   * 3. Define tries and galleryDescText variables.
-   * 4. Try to pull off the galleryDescText while tries are less
-   *    than n and the galleryDescText is still empty. If failed
-   *    reload the page.
-   * 5. Make assertions.
+   * 3. Pull off contents of the selector.
+   * 4. Make assertions.
    */
   await page.goTo();
-
-  const galleryDescSelector = '#root > main > article > div > section';
-
-  const galleryDescText = await page.tryNTimes(
-    async () => await page.getContentsOf(galleryDescSelector)
-  );
-
+  const galleryDescSelector = 'main > article > div > section';
+  const galleryDescText = await page.getContentsOf(galleryDescSelector);
   expect(galleryDescText).toMatch(/portfolio/i);
 });
--- test/logo.test.js
+++ test/logo.test.js
@@ -3,6 +3,9 @@ import { CustomPage } from './helpers/page';
 /**
  * Define page variable in the global scope
  * to make it accessible inside the tests.
+ * @beforeEach - Open the browser and create
+ *               a new tab inside chromium.
+ * @afterEach - Close the browser.
  */
 let page;
 
@@ -11,7 +14,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await page.close(false);
+  await page.close();
 });
 
 /**
@@ -19,13 +22,11 @@ afterEach(async () => {
  */
 it('renders the logo', async () => {
   /**
-   * 1. Navigate to the index page.
-   * 2. Pull out logo contents.
+   * 1. Navigate to /.
+   * 2. Pull off contents of the selector.
    * 3. Make assertions.
    */
   await page.goTo();
-
-  const logoText = await page.getContentsOf('#root > header > div > h1 > a');
-
+  const logoText = await page.getContentsOf('header > div > h1 > a');
   expect(logoText).toMatch(/lost in a postcard/i);
 });
