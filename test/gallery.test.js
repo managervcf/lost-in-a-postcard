b94d31745316192777a17d6c3e48d446f77cee21
Mateusz Pyzowski <managervcf@gmail.com>
2020-09-18 21:37:35 +0800

    Add tests around the gallery component.

--- test/gallery.test.js
+++ test/gallery.test.js
@@ -20,15 +20,65 @@ afterEach(async () => {
 /**
  * Test suites.
  */
-it('renders the gallery description', async () => {
-  /**
-   * 1. Navigate to /.
-   * 2. Define a selector.
-   * 3. Pull off contents of the selector.
-   * 4. Make assertions.
-   */
-  await page.goTo();
-  const galleryDescSelector = 'main > article > div > section';
-  const galleryDescText = await page.getContentsOf(galleryDescSelector);
-  expect(galleryDescText).toMatch(/portfolio/i);
+describe('when navigates to the /photos/featured', () => {
+  beforeEach(async () => {
+    /**
+     * 1. Navigate to /.
+     */
+    await page.goTo();
+  });
+
+  it('renders the gallery', async () => {
+    /**
+     * 1. Define a selector.
+     * 2. Pull off contents of the selector.
+     * 3. Make assertions.
+     */
+    const gallerySelector = '#root > main > article';
+    const galleryText = await page.getContentsOf(gallerySelector);
+    expect(galleryText).toMatch(/portfolio/i);
+  });
+
+  it('renders the gallery description', async () => {
+    /**
+     * 1. Define a selector.
+     * 2. Pull off contents of the selector.
+     * 3. Make assertions.
+     */
+    const galleryDescSelector = 'main > article > div > section';
+    const galleryDescText = await page.getContentsOf(galleryDescSelector);
+    expect(galleryDescText).toMatch(/portfolio/i);
+  });
+});
+
+describe('when navigates to the /photos/:country', () => {
+  beforeEach(async () => {
+    /**
+     * 1. Navigate to /.
+     * 2. Define selectors.
+     * 3. Pull off selectors content.
+     * 4. Navigate to /photos/:country.
+     */
+    await page.goTo();
+    const firstNavLinkSelector =
+      '#root > header > nav > ul > div:nth-child(1) > a';
+    const navLinkContent = await page.getContentsOf(firstNavLinkSelector);
+    await page.goTo(`/photos/${navLinkContent.toLowerCase()}`);
+  });
+
+  it('does not render the gallery description', async () => {
+    /**
+     * 1. Define a selector.
+     * 2. Pull off contents of the selector.
+     * 3. Make assertions.
+     */
+    const galleryDescSelector = 'main > article > div > section';
+    let error = '';
+    try {
+      await page.getContentsOf(galleryDescSelector);
+    } catch (e) {
+      error = e;
+    }
+    expect(error.toString()).toMatch(galleryDescSelector);
+  });
 });
