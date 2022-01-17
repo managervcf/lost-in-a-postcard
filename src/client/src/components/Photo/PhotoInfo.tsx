a36372c3e20606370d5699d38b97a93d523d4ab3
Mateusz Pyzowski <mateuszpyzowski@avertro.com>
2022-01-17 15:30:54 +0800

    Refactor displaying details about a photo.

--- src/client/src/components/Photo/PhotoInfo.tsx
+++ src/client/src/components/Photo/PhotoInfo.tsx
@@ -25,24 +25,26 @@ export const PhotoInfo: React.FC<PhotoInfoProps> = ({
 }) => (
   <section className="gallery-caption-content">
     <p className="gallery-caption-description">
-      <strong>ID:</strong> {id}
+      <strong>AWS key:</strong> {upload.key}
     </p>
     <p className="gallery-caption-description">
-      <strong>AWS key:</strong> {upload.key}
+      <strong>ID:</strong> {id}
     </p>
     <hr />
     <p className="gallery-caption-description">
-      <strong>Caption:</strong> {caption}
+      <strong>Country:</strong> {country.name}
     </p>
     <p className="gallery-caption-description">
       <strong>Region:</strong> {region}
     </p>
     <p className="gallery-caption-description">
-      <strong>Country:</strong> {country?.name}
+      <strong>Caption:</strong> {caption}
     </p>
     <p className="gallery-caption-description">
       <strong>Clicks:</strong> {clicks}
     </p>
-    {featured && <p className="gallery-caption-description">Featured</p>}
+    <p className="gallery-caption-description">
+      <strong>Featured:</strong> {featured ? 'Yes' : 'No'}
+    </p>
   </section>
 );
